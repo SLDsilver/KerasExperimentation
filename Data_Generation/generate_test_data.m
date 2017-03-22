08758000037d17770bab38386e87a478f4bbc76d
SLDsilver <SDsilver@vt.edu>
2017-03-22 14:57:01 -0400

    Better organized files to prevent confusion

--- Data_Generation/generate_test_data.m
+++ Data_Generation/generate_test_data.m
@@ -4,16 +4,16 @@ function generate_test_data(numFiles)
     commandStr = ['rm -r samples/; mkdir samples; cd samples; python ' pwd '/GenIQ.py ' numFiles];
     [status] = system(commandStr);
     if status==0 %If the python script properly executed
-        fprintf('command executed properly');
+        fprintf('.dat files generated properly \n');
     end
     
     for i = 0:(int32(str2double(file_count))-1)
         file_name = ['samples/samples' num2str(i) '.dat'];
-        fprintf(['\n converting: ' file_name])
+        fprintf(['converting: ' file_name '\n'])
         toFile(file_name);
         system(['rm ' file_name]);
     end
-    
+    fprintf('Complete \n')
     system('cd ..');
 
 end
