98d48c67f6f291f28a5fd603edc56043ca1982af
kirito <kirito@vt.edu>
2017-03-22 16:38:34 -0400

    Yes

--- runTHIS.py
+++ runTHIS.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Conv1D
+from keras.layers import Dense, Activation, Convolution1D
 
 import numpy as np
 import correct_Complex as c
@@ -7,19 +7,33 @@ import correct_Complex as c
 
 from pymatbridge import Matlab
 
-#Communicates data extraction from Matlab
-mlab = Matlab()
-mlab.start()
-mlab.run('toFile.m')
+inputfile = ""
+outputfile = ""
+inputtestfile = ""
+outputtestfile = ""
 
-#Import the data 
-c.correct_Complex('usabledata.txt', 'useThis.txt')
-#Convert to numpy array
-c.import_As_Numpy_Array('useThis.txt')
+#Imports the data
+input_train = np.loaddata(inputfile)
+output_train = np.loaddata(outputfile)
+input_test = np.utils.to_categorical(np.loaddata(inputtestfile))
+output_test = np.utils.to_categorical(np.loaddata(outputtestfile))
+num_classes = output_test.shape[1]
 
 
-#How many features map (depth), filters
 #Create the model
-#model = Sequential()
-#model.add(Convolution1D(input_shape=(,1000000), activation='relu',  
+model = Sequential()
+model.add(Convolution1D(9000, 1, 1000, input_shape=(5,1,10000), activation = 'relu')
+model.add(Convolution1D(900, 1, activation = 'relu')
+model.add(Convolution1D(90, 1, activation = 'relu')
+model.add(Flatten())
+model.add(Dense(num_classes, activation = 'softmax'))
+
+#Compile model
+epochs = 25
+lrate = .01
+decay = lrate/ epochs
+sgd = SGD(lr=lrate, momentum = .9, decray = decay, nesterov=False)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+print(model.summary())
     
