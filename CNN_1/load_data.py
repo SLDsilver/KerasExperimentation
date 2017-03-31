c0e8d0376ca271124440bc557fe6e3977d354811
kirito <kirito@vt.edu>
2017-03-31 16:37:49 -0400

    Some more edits. In particular, the model is now correct for the input

--- CNN_1/load_data.py
+++ CNN_1/load_data.py
@@ -2,6 +2,7 @@
 import os
 import numpy as np
 import re
+import random
 input_id = 'samples'
 output_id = 'modScheme'
 #Default form
@@ -29,11 +30,32 @@ def loadData(directory, num_signal, num_samples):
     #print('Resulting output vector: ')
     #print(temp_output)
     return temp_input, temp_output
-'''
-#Allows you specify how much the directory you wish to train against
-def loadData(directory, num_signal, num_samples, percentage):
-    temp_input = np.zeros(shape=(num_signal*percentage), num_samples*2))
-    temp_output = np.zeroes(shape=(int(num_signal*percentage), 1))'''
+
+#Desired Input Shape is (num_signal_total, num_size, 1)
+def loadDataAlpha(directory, num_signal, num_samples, num_size):
+    num_signal_total = num_signal * num_samples / num_size
+    temp_input = np.zeros(shape=(num_signal_total, num_size))
+    temp_output= np.zeros(shape=(num_signal_total, 1))
+    positions = random.sample(range(num_signal_total), num_signal_total)
+    counter = 0
+    print('Processing data')
+    for filename in os.listdir(directory):
+        if input_id in filename:
+            print('Currently processing: ' + filename)
+            fileID = int(re.sub(r"\D", "", filename))
+            temp_data = np.loadtxt(directory + "/" + filename)
+            temp_sol = np.loadtxt(directory + "/" + output_id + str(fileID) + ".txt")
+            temp_data = np.reshape(temp_data, (num_samples / num_size, num_size))
+            for x in range(0, num_samples / num_size):
+                temp_input[positions[counter]] = temp_data[x]
+                temp_output[positions[counter]] = temp_sol
+                counter = counter + 1
+    print('Finished processing data')
+    return temp_input, temp_output
+            
+                                  
+
+
 
 
             
--- CNN_1/runTHIS.py
+++ CNN_1/runTHIS.py
@@ -13,15 +13,15 @@ model_file = 'model_5C.h5'
 
 
 #Load the training tuple
-train_x, train_y = ld.loadData(train_directory, 1000, 10000)
-train_x = np.array(train_x).reshape(1000, 20000, 1)
-train_y = np.array(train_y).reshape(1000, 1)
+train_x, train_y = ld.loadDataAlpha(train_directory, 1000, 20000, 100)
+train_x = np.array(train_x).reshape(200000, 100, 1)
+train_y = np.array(train_y).reshape(200000, 1)
 train_y = np_utils.to_categorical(train_y)
 
 #Load the validation tuple
-test_x, test_y = ld.loadData(test_directory, 20, 10000)
-test_x = np.array(test_x).reshape(20, 20000, 1)
-test_y = np.array(test_y).reshape(20, 1)
+test_x, test_y = ld.loadDataAlpha(test_directory, 20, 20000, 100)
+test_x = np.array(test_x).reshape(4000, 100, 1)
+test_y = np.array(test_y).reshape(4000, 1)
 test_y = np_utils.to_categorical(test_y)
 num_signals = test_y.shape[1]
 
@@ -32,30 +32,29 @@ num_signals = test_y.shape[1]
 
 #Create the model
 model = Sequential()
-model.add(Conv1D(64, 5, activation='relu', input_shape=(20000, 1)))
-model.add(Dropout(.5))
-model.add(Conv1D(64, 5, activation='relu'))
+#Input shape of form: (depth, width, height); For 1D, (width,height)
+model.add(Conv1D(64, 3, activation='relu', input_shape=(100, 1)))
+model.add(Conv1D(64, 3, activation='relu'))
 model.add(MaxPooling1D(3))
-model.add(Conv1D(128, 3, activation='relu'))
-model.add(Dropout(.5))
-model.add(Conv1D(128, 3, activation='relu'))
-model.add(Dropout(.5))
-model.add(Conv1D(5, 1, activation='relu'))
-model.add(Dropout(.5))
+model.add(Dropout(.2))
+model.add(Conv1D(25, 4, activation='relu'))
+model.add(Conv1D(25, 4, activation='relu'))
+model.add(GlobalAveragePooling1D())
+model.add(Dropout(.2))
 model.add(Dense(num_signals,activation='softmax'))
 
 #Compile model
-epoches = 25
+epoches = 10
 lrate = .01
 decay = lrate / epoches
 sgd = SGD(lr = lrate, momentum=.9, decay=decay, nesterov=False)
 model.compile(loss='categorical_crossentropy',
-              optimizer=sgd,
+              optimizer='adam',
               metrics=['accuracy'])
 
 print(model.summary())
 
-model.fit(train_x, train_y, validation_data=(test_x, test_y), epochs=epoches, batch_size = 32)
+model.fit(train_x, train_y, epochs=epoches, batch_size = 32)
 model.save(model_file)
 scores = model.evaluate(test_x, test_y, verbose=0)
 print("Accuracy: %.2f%%" %(scores[1]*100))
